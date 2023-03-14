b1e7b7279090b42193da5cb31cd303266273fe2b
cuinixam <22344617+cuinixam@users.noreply.github.com>
2023-03-14 20:11:54 +0100

    refactor: main view separate frames creation

--- src/candiy/presenter/events.py
+++ src/candiy/presenter/events.py
@@ -3,3 +3,8 @@ from enum import Enum, auto
 
 class EventID(Enum):
     HEARTBEAT = auto()
+    SPY = auto()
+    POWER = auto()
+    TESTER_MODE = auto()
+    XCP_MODE = auto()
+    XCP_GET_ECU_INFO = auto()
--- src/candiy/views/main_view.py
+++ src/candiy/views/main_view.py
@@ -79,24 +79,19 @@ class ToggleButton(customtkinter.CTkButton):
         self._user_command = command
 
 
-class MainView(customtkinter.CTk, View):
+class MainView(View):
     def __init__(self, event_manager: EventManager):
         super().__init__()
+        self.root = customtkinter.CTk()
         self.event_manager = event_manager
         self.messages: Dict[str, TraceViewData] = {}
 
         # configure window
-        self.title("CANDIY")
-        self.geometry(f"{1080}x{580}")
+        self.root.title("CANDIY")
+        self.root.geometry(f"{1080}x{580}")
 
         # update app icon
-        self.iconbitmap(Icons.LOLLIPOP_ICON.file)
-
-        # configure grid layout (1x2)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=0)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=0)
+        self.root.iconbitmap(Icons.LOLLIPOP_ICON.file)
 
         # create menu
         # TODO: Create menu. CTK does not yet have a custom Menu element.
@@ -104,51 +99,103 @@ class MainView(customtkinter.CTk, View):
 
         # ========================================================
         # create main toolbar
-        self.main_toolbar = customtkinter.CTkFrame(self)
-        self.main_toolbar.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+        main_toolbar = customtkinter.CTkFrame(self.root)
+        self.populate_main_toolbar_frame(main_toolbar)
 
-        self.heartbeat_button = ToggleButton(
-            self.main_toolbar,
+        # ========================================================
+        # create tabview and populate with frames
+        tabview = customtkinter.CTkTabview(self.root)
+        self.populate_trace_frame(tabview.add("Trace"))
+        self.populate_diagnostics_frame(tabview.add("Diagnostics"))
+
+        # ========================================================
+        # create statusbar
+        status_frame = customtkinter.CTkFrame(self.root)
+        self.populate_status_frame(status_frame)
+
+        # ========================================================
+        # configure grid
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_rowconfigure(0, weight=0)
+        self.root.grid_rowconfigure(1, weight=1)
+        self.root.grid_rowconfigure(2, weight=0)
+        main_toolbar.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+        tabview.grid(row=1, column=0, padx=10, sticky="nsew")
+        status_frame.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+
+    def populate_main_toolbar_frame(self, frame: customtkinter.CTkFrame) -> None:
+        heartbeat_button = ToggleButton(
+            frame,
             text="Heartbeat",
-            command=self.event_manager.create_event_trigger(EventID.HEARTBEAT),
+            command=self.create_button_event_trigger(EventID.HEARTBEAT),
             image_off=Icons.HEARTBEAT_OFF,
             image_on=Icons.HEARTBEAT_ON,
         )
-        self.heartbeat_button.grid(row=0, column=0)
+        heartbeat_button.grid(row=0, column=0)
 
-        self.spy_button = ToggleButton(
-            self.main_toolbar,
+        spy_button = ToggleButton(
+            frame,
             text="Spy",
-            command=self.toolbar_button_spy_command,
+            command=self.create_button_event_trigger(EventID.SPY),
             image_off=Icons.SPY_OFF,
             image_on=Icons.SPY_ON,
         )
-        self.spy_button.grid(row=0, column=1)
+        spy_button.grid(row=0, column=1)
 
-        self.power_button = ToggleButton(
-            self.main_toolbar,
+        power_button = ToggleButton(
+            frame,
             text="Power",
-            command=self.toolbar_button_power_command,
+            command=self.create_button_event_trigger(EventID.POWER),
             image_off=Icons.POWER_OFF,
             image_on=Icons.POWER_ON,
         )
-        self.power_button.grid(row=0, column=2)
+        power_button.grid(row=0, column=2)
 
-        # ========================================================
-        # create tabview
-        self.tabview = customtkinter.CTkTabview(self)
-        self.tabview.grid(row=1, column=0, padx=10, sticky="nsew")
-        # TODO: add hardware configuration tab
-        # self.tabview.add("Configure")
-        # self.tabview.tab("Configure").grid_columnconfigure(0, weight=1)
-        self.trace_tab = self.tabview.add("Trace")
-        self.diagnostics_tab = self.tabview.add("Diagnostics")
+    def populate_diagnostics_frame(self, frame: customtkinter.CTkFrame) -> None:
+        frame.grid_rowconfigure(0, weight=0)
+        frame.grid_rowconfigure(1, weight=1)
+        frame.grid_columnconfigure(0, weight=0)
+        frame.grid_columnconfigure(1, weight=1)
 
-        # ========================================================
-        # create trace tab
-        self.trace_tab.grid_rowconfigure(0, weight=10)
-        self.trace_tab.grid_rowconfigure(1, weight=1)
-        self.trace_tab.grid_columnconfigure(0, weight=1)
+        # create diagnostics toolbar
+        self.diagnostics_toolbar = customtkinter.CTkFrame(frame)
+        self.diagnostics_toolbar.grid(row=0, column=0, sticky="new", columnspan=2)
+
+        self.tester_mode_button = ToggleButton(
+            self.diagnostics_toolbar,
+            text="Tester mode",
+            command=self.create_button_event_trigger(EventID.TESTER_MODE),
+        )
+        self.tester_mode_button.grid(row=0, column=0)
+
+        self.xcp_mode_button = ToggleButton(
+            self.diagnostics_toolbar,
+            text="XCP mode",
+            command=self.create_button_event_trigger(EventID.XCP_MODE),
+        )
+        self.xcp_mode_button.grid(row=0, column=1)
+
+        # create diagnostics textbox
+        self.diagnostics_textbox = customtkinter.CTkTextbox(
+            frame, activate_scrollbars=True
+        )
+        self.diagnostics_textbox.grid(row=1, column=1, sticky="nsew")
+        self.diagnostics_textbox.insert("0.0", "No network activity...")
+        self.diagnostics_textbox.configure(state="disabled")
+        self.xcp_textbox = customtkinter.CTkTextbox(frame, activate_scrollbars=True)
+        self.xcp_send_ecu_info_button = customtkinter.CTkButton(
+            frame,
+            text="Get ECU Info",
+            command=self.create_button_event_trigger(EventID.XCP_GET_ECU_INFO),
+        )
+        self.xcp_send_ecu_info_button.grid(
+            row=1, column=0, padx=(0, 10), pady=(10, 0), sticky="n"
+        )
+
+    def populate_trace_frame(self, frame: customtkinter.CTkFrame) -> None:
+        frame.grid_rowconfigure(0, weight=10)
+        frame.grid_rowconfigure(1, weight=1)
+        frame.grid_columnconfigure(0, weight=1)
 
         # create trace message tree
         # create a Treeview widget
@@ -166,7 +213,7 @@ class MainView(customtkinter.CTk, View):
 
         # create a Treeview widget
         self.trace_treeview = ttk.Treeview(
-            self.trace_tab,
+            frame,
             columns=columns,
             show="tree headings",
             style="mystyle.Treeview",
@@ -181,90 +228,32 @@ class MainView(customtkinter.CTk, View):
         self.trace_treeview.column(columns[0], minwidth=0, width=150, stretch=NO)
         self.trace_treeview.column(columns[1], minwidth=0, width=50, stretch=NO)
 
-        # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
         # create trace textbox
-        self.trace_textbox = customtkinter.CTkTextbox(
-            self.trace_tab, activate_scrollbars=True
-        )
+        self.trace_textbox = customtkinter.CTkTextbox(frame, activate_scrollbars=True)
         self.trace_textbox.grid(row=1, column=0, sticky="nsew")
         self.trace_textbox.insert("0.0", "No network activity...")
         self.trace_textbox.configure(state="disabled")
 
-        # ========================================================
-        # create diagnostics tab
-        self.diagnostics_tab.grid_rowconfigure(0, weight=0)
-        self.diagnostics_tab.grid_rowconfigure(1, weight=1)
-        self.diagnostics_tab.grid_columnconfigure(0, weight=0)
-        self.diagnostics_tab.grid_columnconfigure(1, weight=1)
-
-        # ========================================================
-        # create diagnostics toolbar
-        self.diagnostics_toolbar = customtkinter.CTkFrame(self.diagnostics_tab)
-        self.diagnostics_toolbar.grid(row=0, column=0, sticky="new", columnspan=2)
-
-        self.tester_mode_button = ToggleButton(
-            self.diagnostics_toolbar,
-            text="Tester mode",
-            command=self.toolbar_button_tester_mode_command,
-        )
-        self.tester_mode_button.grid(row=0, column=0)
-
-        self.xcp_mode_button = ToggleButton(
-            self.diagnostics_toolbar,
-            text="XCP mode",
-            command=self.toolbar_button_xcp_mode_command,
-        )
-        self.xcp_mode_button.grid(row=0, column=1)
-
-        # create diagnostics textbox
-        self.diagnostics_textbox = customtkinter.CTkTextbox(
-            self.diagnostics_tab, activate_scrollbars=True
-        )
-        self.diagnostics_textbox.grid(row=1, column=1, sticky="nsew")
-        self.diagnostics_textbox.insert("0.0", "No network activity...")
-        self.diagnostics_textbox.configure(state="disabled")
-        self.xcp_textbox = customtkinter.CTkTextbox(
-            self.diagnostics_tab, activate_scrollbars=True
-        )
-        self.xcp_send_ecu_info_button = customtkinter.CTkButton(
-            self.diagnostics_tab, text="Get ECU Info"
-        )
-        self.xcp_send_ecu_info_button.grid(
-            row=1, column=0, padx=(0, 10), pady=(10, 0), sticky="n"
-        )
-
-        # ========================================================
-        # create statusbar
-        self.status_frame = customtkinter.CTkFrame(self)
-        self.status_frame.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+    def populate_status_frame(self, status_frame: customtkinter.CTkFrame) -> None:
         self.status_label = customtkinter.CTkLabel(
-            self.status_frame, text="Status unknown..."
+            status_frame, text="Status unknown..."
         )
         self.status_label.grid(row=0, column=0)
 
-    def toolbar_button_heartbeat_command(self) -> None:
-        self.write_toggle_button_status(self.heartbeat_button)
-
-    def toolbar_button_spy_command(self) -> None:
-        self.write_toggle_button_status(self.spy_button)
-
-    def toolbar_button_tester_mode_command(self) -> None:
-        self.write_toggle_button_status(self.tester_mode_button)
-
-    def toolbar_button_power_command(self) -> None:
-        self.write_toggle_button_status(self.power_button)
-
-    def toolbar_button_xcp_mode_command(self) -> None:
-        self.write_toggle_button_status(self.xcp_mode_button)
-
-    def write_toggle_button_status(self, button: ToggleButton) -> None:
-        self.status_label.configure(
-            text=f"{button.cget('text')} button is {'ON' if button.state else 'OFF' }"
-        )
-
     def update_text(self, text: str) -> None:
         return self.status_label.configure(text=text)
+
+    def mainloop(self) -> None:
+        return self.root.mainloop()
+
+    def create_button_event_trigger(self, event_id: EventID) -> Callable[[], None]:
+        event_trigger = self.event_manager.create_event_trigger(event_id)
+
+        def create_callback() -> None:
+            self.update_text(f"Event {event_id.name} triggered.")
+            event_trigger()
+
+        return create_callback
 
     def update_trace_view(self, messages: Dict[str, TraceViewData]) -> None:
         self.messages.update(messages)
